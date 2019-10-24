567bdac4f8e19e688f176805cf41f01fe2773176
Yasset Perez-Riverol <ypriverol@gmail.com>
2019-10-24 15:30:38 +0100

    proxi implementation for spectra

--- src/main/java/uk/ac/ebi/pride/ws/pride/assemblers/TransformerMongoSpectra.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/assemblers/TransformerMongoSpectra.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.pride.ws.pride.assemblers;
 
+import lombok.extern.slf4j.Slf4j;
 import uk.ac.ebi.pride.archive.dataprovider.data.peptide.PSMProvider;
 import uk.ac.ebi.pride.archive.spectra.model.ArchiveSpectrum;
 import uk.ac.ebi.pride.ws.pride.models.ISpectrum;
@@ -10,6 +11,7 @@ import java.util.function.Function;
 /**
  *
  */
+@Slf4j
 public class TransformerMongoSpectra implements Function<PSMProvider, ISpectrum> {
 
     @Override
@@ -17,6 +19,7 @@ public class TransformerMongoSpectra implements Function<PSMProvider, ISpectrum>
         ArchiveSpectrum spectrum = ((ArchiveSpectrum) psmProvider);
         Double[] mzs = spectrum.getMasses();
         Double[] intensities = spectrum.getIntensities();
+        log.debug("Spectra retrirved -- " + spectrum.getUsi());
 
         return Spectrum.builder()
                 .usi(spectrum.getUsi())
